bf38e3f722100c78e1c48e0a385fd3ffb1238aa6
Radek <rzacharzewski@objectivity.co.uk>
2019-08-16 09:18:28 +0200

    End of coding ex 3

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,4 +1,4 @@
-var avgScoreJohn = (150+120+103)/3;
+/*var avgScoreJohn = (150+120+103)/3;
 var avgScoreMark = (116+94+123)/3;
 var avgScoreMary = (97+134+105)/3;
 
@@ -17,3 +17,28 @@ else if (avgScoreMary > avgScoreJohn && avgScoreMary > avgScoreMark) {
 }
     else
     console.log('Draw');
+
+*/
+
+var bills = [124,48,268];
+
+
+var tip = function(bill) {
+    switch(true) {
+        case bill < 50:
+            return bill * 0.2;
+        case bill >= 50 && bill < 200:
+            return bill * 0.15;
+        case bill >= 200:
+            return bill * 0.1;
+        default:
+            return 0;
+    }
+}
+        
+var tips = [tip(bills[0]),tip(bills[1]),tip(bills[2])];
+var totals = [(bills[0]+tips[0]),(bills[1]+tips[1]),(bills[2]+tips[2])]
+
+console.log(bills);
+console.log(tips);
+console.log(totals);
